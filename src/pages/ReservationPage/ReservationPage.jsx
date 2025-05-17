940990aee1ceb97109f983d9839dddfb6261e139
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-17 16:36:45 +0200

    Add reservation page & logic for display actual data

--- src/pages/ReservationPage/ReservationPage.jsx
+++ src/pages/ReservationPage/ReservationPage.jsx
@@ -1,3 +1,48 @@
+import { useEffect, useState } from 'react';
+import './style.css';
+import { useParams } from 'react-router';
+
 export const ReservationPage = () => {
-  return <h2>Vaše e-jízdenka</h2>;
+  const [reservation, setReservation] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchTicketDetail = async () => {
+      const response = await fetch(
+        `https://apps.kodim.cz/daweb/leviexpress/api/reservation?id=${params.id}`,
+      );
+      const responseData = await response.json();
+      const data = responseData.results;
+      setReservation(data);
+    };
+    fetchTicketDetail();
+  }, []);
+
+  console.log('Co je v data', reservation);
+
+  return (
+    reservation && (
+      <div className="reservation container">
+        <h2>Vaše e-jízdenka č. {reservation.reservationId}</h2>
+        <div className="reservation__body">
+          <div className="reservation__headings">
+            <p>Datum cesty: </p>
+            <p>Odjezd:</p>
+            <p>Příjezd:</p>
+            <p>Sedadlo:</p>
+          </div>
+          <div className="reservation__info">
+            <p>{reservation.date}</p>
+            <p>
+              {reservation.fromCity.name}, {reservation.fromCity.time}
+            </p>
+            <p>
+              {reservation.toCity.name}, {reservation.toCity.time}
+            </p>
+            <p>{reservation.seatNumber}</p>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
